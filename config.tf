7f679611d86a77a4d45d00a1828d2b263f8fc0ac
charles-d-burton <charles.burton@mindflash.com>
2017-12-05 18:23:22 -0700

    added policy for vpc cluster

--- config.tf
+++ config.tf
@@ -4,6 +4,8 @@ provider "aws" {
 
 # cluster policy document
 data "aws_iam_policy_document" "elk_cluster" {
+  count = "${var.use_vpc ? 0 : 1}"
+
   statement {
     actions = [
       "es:*",
@@ -64,6 +66,88 @@ data "aws_iam_policy_document" "elk_cluster" {
     resources = [
       "${aws_elasticsearch_domain.elk.arn}/*",
       "${aws_elasticsearch_domain.elk.arn}",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "IpAddress"
+      variable = "aws:SourceIp"
+
+      values = ["${var.ip_whitelist}"]
+    }
+  }
+}
+
+# cluster policy document
+data "aws_iam_policy_document" "elk_cluster_vpc" {
+  count = "${var.use_vpc ? 1 : 0}"
+
+  statement {
+    actions = [
+      "es:*",
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = ["${var.es_publish_role_arns}"]
+    }
+
+    resources = [
+      "${aws_elasticsearch_domain.elk_vpc.arn}/*",
+      "${aws_elasticsearch_domain.elk_vpc.arn}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "es:*",
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "${aws_iam_role.logger.arn}",
+      ]
+    }
+
+    resources = [
+      "${aws_elasticsearch_domain.elk_vpc.arn}/*",
+      "${aws_elasticsearch_domain.elk_vpc.arn}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "es:ESHttpGet",
+      "es:ESHttpHead",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = "${var.kibana_role_arns}"
+    }
+
+    resources = [
+      "${aws_elasticsearch_domain.elk_vpc.arn}/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "es:ESHttp*",
+    ]
+
+    resources = [
+      "${aws_elasticsearch_domain.elk_vpc.arn}/*",
+      "${aws_elasticsearch_domain.elk_vpc.arn}",
     ]
 
     effect = "Allow"
