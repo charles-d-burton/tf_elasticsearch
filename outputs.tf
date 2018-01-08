69356b20535e128cba8593f7d146d364d9eab6be
charles-d-burton <charles.burton@mindflash.com>
2018-01-08 11:37:04 -0700

    fix: outputs now output string from coalesced list

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "domain_arn" {
-  value = "${var.use_vpc ? aws_elasticsearch_domain.elk_vpc.arn : aws_elasticsearch_domain.elk.arn }"
+  value = "${coalesce(aws_elasticsearch_domain.elk_vpc.arn, aws_elasticsearch_domain.elk.arn)}"
 }
 
 output "domain_endpoint" {
-  value = "${var.use_vpc ? aws_elasticsearch_domain.elk_vpc.endpoint : aws_elasticsearch_domain.elk.endpoint }"
+  value = "${coalesce(aws_elasticsearch_domain.elk_vpc.endpoint, aws_elasticsearch_domain.elk.endpoint)}"
 }
 
 output "domain_id" {
-  value = "${var.use_vpc ? aws_elasticsearch_domain.elk_vpc.domain_id : aws_elasticsearch_domain.elk.domain_id }"
+  value = "${coalesce(aws_elasticsearch_domain.elk_vpc.domain_id, aws_elasticsearch_domain.elk.domain_id)}"
 }
 
 output "logger_role_arn" {
