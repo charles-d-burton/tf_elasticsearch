68a7a2f7345511d97951f7f32cc8e230121d5aae
chris ludden <chris.ludden@gmail.com>
2018-01-08 10:22:25 -0700

    fix: updates outputs to use correct cluster info based on `use_vpc` variable

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "domain_arn" {
-  value = "${aws_elasticsearch_domain.elk.arn}"
+  value = "${var.use_vpc ? aws_elasticsearch_domain.elk_vpc.arn : aws_elasticsearch_domain.elk.arn }"
 }
 
 output "domain_endpoint" {
-  value = "${aws_elasticsearch_domain.elk.endpoint}"
+  value = "${var.use_vpc ? aws_elasticsearch_domain.elk_vpc.endpoint : aws_elasticsearch_domain.elk.endpoint }"
 }
 
 output "domain_id" {
-  value = "${aws_elasticsearch_domain.elk.domain_id}"
+  value = "${var.use_vpc ? aws_elasticsearch_domain.elk_vpc.domain_id : aws_elasticsearch_domain.elk.domain_id }"
 }
 
 output "logger_role_arn" {
