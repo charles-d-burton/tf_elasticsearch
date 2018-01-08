0ba146620965d9cf9c96ba0723490079d3a4e391
charles-d-burton <charles.burton@mindflash.com>
2018-01-08 12:09:49 -0700

    fix: Workaround for terrafor issue #11566

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "domain_arn" {
-  value = "${coalesce(aws_elasticsearch_domain.elk_vpc.arn, aws_elasticsearch_domain.elk.arn)}"
+  value = "${coalesce(join("", aws_elasticsearch_domain.elk_vpc.*.arn), join("", aws_elasticsearch_domain.elk.*.arn))}"
 }
 
 output "domain_endpoint" {
-  value = "${coalesce(aws_elasticsearch_domain.elk_vpc.endpoint, aws_elasticsearch_domain.elk.endpoint)}"
+  value = "${coalesce(join("", aws_elasticsearch_domain.elk_vpc.*.endpoint), join("", aws_elasticsearch_domain.elk.*.endpoint))}"
 }
 
 output "domain_id" {
-  value = "${coalesce(aws_elasticsearch_domain.elk_vpc.domain_id, aws_elasticsearch_domain.elk.domain_id)}"
+  value = "${coalesce(join("", aws_elasticsearch_domain.elk_vpc.*.domain_id), join("", aws_elasticsearch_domain.elk.*.domain_id))}"
 }
 
 output "logger_role_arn" {
